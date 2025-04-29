50a8cb7080b53d1b4ecafeb73572021232e2d3a3
giosuel <sirtubelujohnson@gmail.com>
2025-04-29 18:43:42 +0200

    BepInEx fix

--- Librarium/src/Librarium.cs
+++ Librarium/src/Librarium.cs
@@ -3,4 +3,10 @@ using BepInEx;
 namespace Librarium;
 
 [BepInPlugin(LCMPluginInfo.PLUGIN_GUID, LCMPluginInfo.PLUGIN_NAME, LCMPluginInfo.PLUGIN_VERSION)]
-public class Librarium;
+public class Librarium : BaseUnityPlugin
+{
+    private void Awake()
+    {
+        Logger.LogInfo("Librarium has been loaded successfully! \\o/");
+    }
+}
